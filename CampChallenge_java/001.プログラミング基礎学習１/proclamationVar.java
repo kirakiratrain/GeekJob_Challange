787de3909b5ff4e782ac25d8032fe3145614d6bf
Yuichi Ito <kirakiratrain1103@gmail.com>
2017-02-08 20:00:37 +0900

    変数の宣言と表示(自己紹介)

---
+++ CampChallenge_java/001.プログラミング基礎学習１/proclamationVar.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Choir
+ */
+public class proclamationVar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        //自己紹介
+        //名前
+        String Name = "伊藤　雄一";
+        //出身
+        String from = "宮城";
+        //年齢
+        String age = "28";
+        //職業
+        String occupation = "SE";
+        
+        System.out.println("自己紹介");
+        System.out.println("名前："+ Name);
+        System.out.println("出身："+from);
+        System.out.println("年齢："+age);
+        System.out.println("職業："+occupation);
+        
+    }
+    
+}
