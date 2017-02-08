e99c035138a560770235d01da94a03eff302c99f
Yuichi Ito <kirakiratrain1103@gmail.com>
2017-02-08 19:36:57 +0900

    文字の連結表示

---
+++ CampChallenge_java/001.プログラミング基礎学習１/LinkingViewChar.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Choir
+ */
+public class LinkingViewChar
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        String clink;
+        clink = "groove" + "-" + "gear";
+        
+        System.out.printf(clink);
+    
+        
+        
+        
+    }
+    
+}
