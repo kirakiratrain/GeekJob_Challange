2ac0766b3ff59674e6c272fa05a2a6ca15bc7d84
Yuichi Ito <kirakiratrain1103@gmail.com>
2017-02-16 21:33:53 +0900

    オブジェクト指向1-クラスの継承

---
+++ CampChallenge_java/005.オブジェクト指向１/objectStudy/src/java/org/objectStudy/classMake/classMake_clear.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.objectStudy.classMake;
+import org.objectStudy.classMake.classMake;
+
+/**
+ *
+ * @author Choir
+ */
+public class classMake_clear extends classMake
+{
+    public classMake_clear(){};
+    
+    public void clearData()
+    {
+        hdd = 0;
+        color = "";
+    };
+}
