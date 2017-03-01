99fb120011be19e13873d09c7b54f222153f01cc
Yuichi Ito <kirakiratrain1103@gmail.com>
2017-03-01 14:05:16 +0900

    エラーハンドリングを含んだ接続の確立

---
+++ CampChallenge_java/010.JavaからのDB操作/Java_DB/src/java/org/database/connect/Servlet_ConnectDB.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.database.connect;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author Choir
+ */
+public class Servlet_ConnectDB extends HttpServlet 
+{
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        //DB
+        Connection db_con = null;
+        PreparedStatement db_st = null;
+        ResultSet db_data = null;
+        
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:8889/Challenge_db","root","root");
+            db_st = db_con.prepareStatement("select * FROM user where userID = 1");
+            db_data = db_st.executeQuery();
+    
+            while(db_data.next())
+            {
+                String str = db_data.getString("name");
+                out.print("userID:" + db_data.getString("userID") + "<br>");
+                out.print("name:"   + db_data.getString("name")   + "<br>");
+                out.print("age:"    + db_data.getString("age")    + "<br>");
+            }
+            
+            db_con.close();
+            db_st.close();
+            db_data.close();
+        }
+        catch(SQLException e_sql)
+        {
+            out.println("データベースの接続に失敗しました！"+e_sql.getMessage());
+        }
+        catch(Exception e)
+        {
+            out.println("データベースの接続に失敗しました！"+e.getMessage());
+        }
+        finally
+        {
+            if(db_con != null)
+            {
+                try
+                {
+                    db_con.close();
+                }
+                catch(Exception e)
+                {
+                    System.out.print("con:"+e.getMessage());
+                }
+            }
+            if(db_st != null)
+            {
+                try
+                {
+                    db_st.close();
+                }
+                catch(Exception e)
+                {
+                    out.println("st:"+e.getMessage());
+                }
+            }
+            if(db_data != null)
+            {
+                try
+                {
+                    db_data.close();
+                }
+                catch(Exception e)
+                {
+                    out.println("data:"+e.getMessage());
+                }
+                 
+            }
+        }
+        
+//        try (PrintWriter out = response.getWriter()) 
+//        {
+//            /* TODO output your page here. You may use following sample code. */
+//            
+//            
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet Servlet_ConnectDB</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet Servlet_ConnectDB at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
